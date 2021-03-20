f655e1521f355f4bb15dd33aba87765147c07c46
Nolan Chow <72522731+Nolan-Chow@users.noreply.github.com>
2021-03-20 12:38:07 -0700

    implement local strategy (using fake database)

--- reminderAppStarterFile/controller/auth_controller.js
+++ reminderAppStarterFile/controller/auth_controller.js
@@ -1,22 +1,23 @@
-let database = require("../database");
+const passport = require("../middleware/passport");
 
 let authController = {
   login: (req, res) => {
-    //TODO change login to auth/login by implementing an authRoute
-    // in the meantime, this works when getting to the homepage
-    res.render("login");
+    res.render("auth/login");
   },
 
   register: (req, res) => {
     res.render("auth/register");
   },
 
-  loginSubmit: (req, res) => {
-    // implement
+  loginSubmit: (req, res, next) => {
+    passport.authenticate("local", {
+      successRedirect: "/reminders",
+      failureRedirect: "/login",
+    })(req, res, next);
   },
 
   registerSubmit: (req, res) => {
-    // implement
+    
   },
 };
 
